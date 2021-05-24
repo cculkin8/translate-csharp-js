530642dc5a0f860d960391fe3b4c5ed7cf264eb7
Carter <carter.culkin@gmail.com>
2021-05-24 13:59:22 -0500

    deaf people can have a spell cast to make them hear instead of heal now, also this is working in javascript now

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,41 @@
-// Put your code here
+const spells = [
+    {
+        name: "Turn enemy into a newt",
+        IsEvil: true,
+        EnergyReqired: 5.1
+    },
+    {
+        name: "Conjure some gold for a local charity",
+        IsEvil: false,
+        EnergyReqired: 2.99
+    },
+    {
+        name: "Give a deaf person the ability to hear",
+        IsEvil: false,
+        EnergyReqired: 12.2
+    },
+    {
+        name: "Make yourself emperor of the universe",
+        IsEvil: true,
+        EnergyReqired: 100.0
+    },
+    {
+        name: "Convince your relatives your political views are correct",
+        IsEvil: false,
+        EnergyReqired: 8888.8
+    }
+]
+
+console.log("Do you believe in magic?");
+
+const spellbook =() =>{
+    let goodbook = spells.filter(spell => !spell.IsEvil)
+    console.log("Good Book")
+    goodbook.forEach(spell => console.log(`   ${spell.name}`))
+    console.log("")
+    let evilbook = spells.filter(spell => spell.IsEvil)
+    console.log("Evil Book")
+    evilbook.forEach(spell => console.log(`   ${spell.name}`))
+}
+
+spellbook()
